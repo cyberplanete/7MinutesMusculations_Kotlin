f960b8c1c1f2633b06fc64f66c59447ec3cedb69
pascal <pascal.joret@gmail.com>
2022-04-28 15:20:23 +0200

    Creation modèle point de progression

--- app/src/main/java/net/cyberplanete/a7minutesWorkout_kotlin/MainActivity.kt
+++ app/src/main/java/net/cyberplanete/a7minutesWorkout_kotlin/MainActivity.kt
@@ -7,7 +7,7 @@ import android.widget.Toast
 import net.cyberplanete.a7minutesWorkout_kotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private var bindingViewMainActivity : ActivityMainBinding? = null
+    private var bindingViewMainActivity: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,22 +17,21 @@ class MainActivity : AppCompatActivity() {
         bindingViewMainActivity = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingViewMainActivity?.root)
 
-        bindingViewMainActivity?.frameLayoutStart?.setOnClickListener{
-            Toast.makeText(this,"Here we will start the exercise", Toast.LENGTH_SHORT).show()
+        bindingViewMainActivity?.frameLayoutStart?.setOnClickListener {
+            Toast.makeText(this, "Here we will start the exercise", Toast.LENGTH_SHORT).show()
 
             /**
              * Démarrer la page ExcerciseActivity
-              */
-        val intentExerciceActivity = Intent(this, ExerciseActivity::class.java)
-        startActivity(intentExerciceActivity)
+             */
+            val intentExerciceActivity = Intent(this, ExerciseActivity::class.java)
+            startActivity(intentExerciceActivity)
         }
     }
 
     /**
      * To avoid memory leak
-      */
-    override fun onDestroy()
-    {
+     */
+    override fun onDestroy() {
         super.onDestroy()
         bindingViewMainActivity = null
     }
