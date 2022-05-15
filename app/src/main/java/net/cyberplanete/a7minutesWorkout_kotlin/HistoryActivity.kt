720aef603f78603344ae8c6db014a7314fc4c21a
pascal <pascal.joret@gmail.com>
2022-05-15 20:22:15 +0200

    Creation de la logique pour avoir un historique des exercices réalisés

--- app/src/main/java/net/cyberplanete/a7minutesWorkout_kotlin/HistoryActivity.kt
+++ app/src/main/java/net/cyberplanete/a7minutesWorkout_kotlin/HistoryActivity.kt
@@ -2,9 +2,17 @@ package net.cyberplanete.a7minutesWorkout_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import net.cyberplanete.a7minutesWorkout_kotlin.databinding.ActivityHistoryBinding
-import net.cyberplanete.a7minutesWorkout_kotlin.useful.Constants
+import net.cyberplanete.a7minutesWorkout_kotlin.adapteurs.HistoryAdapteur
+import net.cyberplanete.a7minutesWorkout_kotlin.dao.HistoryDAO
+import net.cyberplanete.a7minutesWorkout_kotlin.models.HistoryEntity
 
 class HistoryActivity : AppCompatActivity() {
 
@@ -14,6 +22,16 @@ class HistoryActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         bindingHistory = ActivityHistoryBinding.inflate(layoutInflater)
         setContentView(bindingHistory?.root)
+
+        val historyDAO = (application as HistoryApp).historyDatabase.historyDAO()
+
+        lifecycleScope.launch {
+            historyDAO.fetchAllHistory().collect {
+                val historyList = ArrayList(it)
+                setupHistoryListIntoRecyclerView(historyList, historyDAO)
+            }
+        }
+
 
         /* Action BAR*/
         setSupportActionBar(bindingHistory?.toolbarHistory)
@@ -25,15 +43,89 @@ class HistoryActivity : AppCompatActivity() {
         /* Bouton retour   <-  vers le début de l'exercice  */
         bindingHistory?.toolbarHistory?.setNavigationOnClickListener()
         {
-            //onBackPressed()
-            customDialogForBackButton()
+            onBackPressed()
+            //  customDialogForBackButton()
 
         }
         /* END - Action BAR*/
 
 
+    }
+
+    //Logique pour l'affichage de la liste
+    private fun setupHistoryListIntoRecyclerView(
+        historyList: ArrayList<HistoryEntity>,
+        historyDAO: HistoryDAO
+    ) {
+        if (historyList.isNotEmpty()) {
+            val anHistoryRow = HistoryAdapteur(historyList) { deleteHistoryRow ->
+                lifecycleScope.launch {
+                    historyDAO.fetchAnHistoryByDate(deleteHistoryRow).collect {
+                        if (it != null) {
+                            deleteRecordAlertDialog(deleteHistoryRow, historyDAO, it )
+                        }
+                    }
+                }
+            }
+            bindingHistory?.rvHistoryList?.layoutManager = LinearLayoutManager(this)
+            bindingHistory?.rvHistoryList?.adapter = anHistoryRow
+            bindingHistory?.rvHistoryList?.visibility = View.VISIBLE
+        } else {
+            bindingHistory?.rvHistoryList?.visibility = View.GONE
+        }
+    }
+
+    /*
+UI Alert Dialog - deleteRecordAlertDialog
+ */
+    private fun deleteRecordAlertDialog(
+        date: String,
+        historiqueDAO: HistoryDAO,
+        historyEntity: HistoryEntity
+    ) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Delete Record")
+        //set message for alert dialog
+        builder.setMessage("Are you sure you wants to delete ${historyEntity.date}.")
+        builder.setIcon(android.R.drawable.ic_dialog_alert)
+
+        /*
+        performing positive action
+         */
+        builder.setPositiveButton("Yes") { monDialog, _ ->
+            lifecycleScope.launch {
+                historiqueDAO.delete(HistoryEntity(date))
+                Toast.makeText(
+                    applicationContext,
+                    "Record deleted successfully.",
+                    Toast.LENGTH_LONG
+                ).show()
+
+                monDialog.dismiss() // Dialog will be dismissed
+            }
+        }
+        /*
+      END performing positive action
+      */
+
+        /*
+         performing negative action
+        */
+        builder.setNegativeButton("No") { dialog, which ->
+            dialog.dismiss()
+        }
+
+        /*
+        *  FINALISATION DE L'ALERTE DIALOG
+        * */
+        val alertDialogForDeleteRecord: AlertDialog = builder.create()
+        alertDialogForDeleteRecord.setCancelable(false)
+        alertDialogForDeleteRecord.show()
 
     }
+
+
+
 
     private fun customDialogForBackButton() {
         TODO("Not yet implemented")
